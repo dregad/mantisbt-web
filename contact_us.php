727f2ea3894f8a0e5df41c83bf57dd394045ea72
Damien Regad <dregad@mantisbt.org>
2013-07-23 18:20:21 +0200

    form validation

--- contact_us.php
+++ contact_us.php
@@ -5,10 +5,45 @@
 	$t_sub_title = "Contact Us";
 	include( "top.php" );
 
-	$f_contact_name = gpc_strip_slashes( isset( $_POST['contact_name'] ) ? $_POST['contact_name'] : '' );
-	$f_contact_email = gpc_strip_slashes( isset( $_POST['contact_email'] ) ? $_POST['contact_email'] : '' );
-	$f_subject = gpc_strip_slashes( isset( $_POST['subject'] ) ? $_POST['subject'] : '' );
-	$f_email_body = gpc_strip_slashes( isset( $_POST['email_body'] ) ? $_POST['email_body'] : '' );
+	$t_msg = 'For any questions related to usage or configuration of MantisBT,
+		kindly use our <a href="http://www.mantisbt.org/forums/">forums</a>.
+		Please understand that the MantisBT developers cannot offer personal support.';
+
+	# Retrieve form variables
+	$f_contact_name  = trim( gpc_strip_slashes( isset( $_POST['contact_name'] ) ? $_POST['contact_name'] : '' ) );
+	$f_contact_email = trim( gpc_strip_slashes( isset( $_POST['contact_email'] ) ? $_POST['contact_email'] : '' ) );
+	$f_subject       = trim( gpc_strip_slashes( isset( $_POST['subject'] ) ? $_POST['subject'] : '' ) );
+	$f_email_body    = trim( gpc_strip_slashes( isset( $_POST['email_body'] ) ? $_POST['email_body'] : '' ) );
+
+	# Form was submitted
+	if( isset( $_POST["recaptcha_response_field"] ) ) {
+		# Check that all required fields were provided
+		if(    empty( $f_contact_name )
+			|| empty( $f_contact_email )
+			|| empty( $f_subject )
+			|| empty( $f_email_body )
+		) {
+			$t_msg = "Please fill in all the fields !";
+		}
+		# Validate the captcha
+		else {
+			$t_resp = recaptcha_check_answer(
+				$t_recaptcha_private_key,
+				$_SERVER["REMOTE_ADDR"],
+				$_POST["recaptcha_challenge_field"],
+				$_POST["recaptcha_response_field"]
+			);
+
+			if( $t_resp->is_valid ) {
+				echo "valid captcha";
+### @TODO: send mail now
+			} else {
+				$t_msg = "The reCAPTCHA wasn't entered correctly, please go back and try again.";
+			}
+		}
+	}
+
+
 ?>
 
 <!-- recaptcha layout options -->
@@ -18,17 +53,16 @@
 	};
 </script>
 
-
 <h4>Contact Us</h4>
 
 <p align="left">Please use the form below to email us with any queries that you may have.</p>
 
-<div class="spotlight">
-<p><big><strong><font color="red">For questions relating to how to use or configure MantisBT, please use the <a href="http://www.mantisbt.org/forums/">forums</a>. Please understand that the MantisBT developers cannot offer personal support.</font></strong></big></p>
+<div class="spotlight"  >
+	<p style="font-size: medium; text-weight: bold; color: red;"><?php echo $t_msg; ?></p>
 </div>
 
 <br />
-<form name="frmAddEntry" method="post" action="contact_send.php">
+<form name="frmAddEntry" method="post">
 
 	<table width="100%" border="0" cellspacing="0" cellpadding="0">
 		<tr>
@@ -55,7 +89,6 @@
 			<td style="padding:10px;">&nbsp;</td>
 			<td style="padding:10px;">
 				<input name="admAdd" type="submit" id="admAdd" value="Submit" />
-				<input name="issubmit" type="hidden" value="1" />
 			</td>
 		</tr>
 	</table>
