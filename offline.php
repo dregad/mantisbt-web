bc70c6c92a0669aff6a1b1466c87c2609f8e65f0
Damien Regad <dregad@mantisbt.org>
2014-03-11 14:30:59 +0100

    Added offline page
    The purpose is to have Apache redirect to this page when parts of the
    site are down for maintenance.
    
    Key elements from top.php were copied to this file and links changed to
    absolute from Document Root so they work even if called from another
    directory in the site

---
+++ offline.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>Mantis Bug Tracker<?php if ( isset( $t_sub_title ) ) { echo " | $t_sub_title"; } ?></title>
+
+	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+
+	<!-- META HTTP-EQUIV -->
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	<meta http-equiv="content-language" content="en-us" />
+
+	<link rel="stylesheet" href="/css/mantisbt.css" />
+	<link rel="stylesheet" href="/css/bootstrap.min.css" />
+	<link rel="stylesheet" href="/css/custom.css" />
+	<link rel="stylesheet" href="/css/color_scheme.css" />
+	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400,400italic,600,600italic,700,700italic" type="text/css" />
+
+</head>
+
+<body>
+
+<div>
+	<div id="out_container">
+		<!-- THE LINE AT THE VERY TOP OF THE PAGE -->
+		<div class="top_line"></div>
+		<!-- HEADER AREA -->
+		<header>
+			<div class="container">
+				<div class="row">
+					<!-- HEADER: LOGO AREA -->
+					<div class="col-xs-12 col-sm-4 col-md-3 logo">
+						<a class="logo" href="/index.php" onclick="ga('send', 'event', 'Header', 'Logo');">
+							<img alt="logo" title="Mantis Bug Tracker" width="230" src="/images/mantis_logo_262x90.png" class="img-responsive" />
+						</a>
+					</div>
+				</div>
+			</div>
+		</header>
+
+		<div class="main-content">
+			<div class="container">
+				<br />
+				<h1>This page is temporarily unavailable</h1>
+				<p>We apologize for the inconvenience. Please come back later.</p>
+				<p>Go back to <a href="/index.php">mantisbt.org home page</a></p>
+
+			</div>
+		</div>
+
+	</div>
+</div>
+
+</body>
+</html>
